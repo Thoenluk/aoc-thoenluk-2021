22c3f2918ea64046789ef472e79047e0070f74e4
thoenluk <thoenluk@students.zhaw.ch>
2021-12-11 12:28:05 +0100

    Day 11: Solve day 11.
    Feed crabs.
    
    Pet octopuses.

---
+++ aoc2021/src/main/java/thoenluk/aoc2021/challenge11/OctopusChargingStation.java
@@ -0,0 +1,78 @@
+package thoenluk.aoc2021.challenge11;
+
+import thoenluk.aoc2021.ChristmasSaver;
+import thoenluk.aoc2021.ut.Position;
+import thoenluk.aoc2021.ut.Ut;
+
+import java.util.*;
+
+import static thoenluk.aoc2021.ut.Position.NeighbourDirection.OMNIDIRECTIONAL;
+
+public class OctopusChargingStation implements ChristmasSaver {
+    @Override
+    public String saveChristmas(String input) {
+        final Map<Position, Integer> octopuses = Ut.multilineStringToPositionIntegerMap(input);
+        final List<Position> octopodesReadyToFlash = new LinkedList<>();
+        int totalFlashes = 0;
+
+        for (int i = 0; i < 100; i++) {
+            chargeOctoghettiNaturally(octopuses, octopodesReadyToFlash);
+
+            totalFlashes += processCrossFlashtamination(octopuses, octopodesReadyToFlash);
+        }
+
+        return Integer.toString(totalFlashes);
+    }
+
+    private void chargeOctoghettiNaturally(Map<Position, Integer> octopuses, List<Position> octopodesReadyToFlash) {
+        octopuses.replaceAll((position, charge) -> {
+            charge++;
+            if (charge > 9) {
+                octopodesReadyToFlash.add(position);
+            }
+            return charge;
+        });
+    }
+
+    private int processCrossFlashtamination(Map<Position, Integer> octopuses, List<Position> octopodesReadyToFlash) {
+        final Set<Position> octopiThatFlashed = new HashSet<>();
+        Position octopus;
+        int neighbourCharge;
+
+        while (!octopodesReadyToFlash.isEmpty()) {
+            octopus = octopodesReadyToFlash.remove(0);
+            octopuses.put(octopus, 0);
+            octopiThatFlashed.add(octopus);
+
+            for (Position neighbour : octopus.getNeighbours(OMNIDIRECTIONAL)) {
+                if (octopiThatFlashed.contains(neighbour) || !octopuses.containsKey(neighbour)) {
+                    continue;
+                }
+                neighbourCharge = octopuses.get(neighbour);
+                neighbourCharge++;
+                octopuses.put(neighbour, neighbourCharge);
+                if (neighbourCharge > 9) {
+                    if(!octopodesReadyToFlash.contains(neighbour)) {
+                        octopodesReadyToFlash.add(neighbour);
+                    }
+                }
+            }
+        }
+
+        return octopiThatFlashed.size();
+    }
+
+    @Override
+    public String saveChristmasAgain(String input) {
+        final Map<Position, Integer> octopuses = Ut.multilineStringToPositionIntegerMap(input);
+        final List<Position> octopodesReadyToFlash = new LinkedList<>();
+
+        for (int i = 1; true; i++) {
+            chargeOctoghettiNaturally(octopuses, octopodesReadyToFlash);
+
+            if (processCrossFlashtamination(octopuses, octopodesReadyToFlash) == octopuses.size()) {
+                return Integer.toString(i);
+            }
+        }
+    }
+}
