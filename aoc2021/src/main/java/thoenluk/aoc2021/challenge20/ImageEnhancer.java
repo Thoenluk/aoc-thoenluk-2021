c7e393174a99608ee56c699f0d30a9e66f8b5585
thoenluk <thoenluk@students.zhaw.ch>
2021-12-20 15:51:31 +0100

    Day 20: Solve day 20.
    Refactor Position to deliver neighbours in-order because it turns out that matters sometimes.
    
    Feed crabs.
    
    Feed and pet octopus. Continue mural using ink conveniently provided.
    
    Lock down submarine and engage security system against nebula.

---
+++ aoc2021/src/main/java/thoenluk/aoc2021/challenge20/ImageEnhancer.java
@@ -0,0 +1,91 @@
+package thoenluk.aoc2021.challenge20;
+
+import thoenluk.aoc2021.ChristmasSaver;
+import thoenluk.aoc2021.ut.Position;
+import thoenluk.aoc2021.ut.Ut;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static thoenluk.aoc2021.ut.Position.NeighbourDirection.OMNIDIRECTIONAL_AND_SELF;
+
+public class ImageEnhancer implements ChristmasSaver {
+    @Override
+    public String saveChristmas(String input) {
+        return Integer.toString(solveForIterationCount(input, 2));
+    }
+
+    @Override
+    public String saveChristmasAgain(String input) {
+        return Integer.toString(solveForIterationCount(input, 50));
+    }
+
+    private int solveForIterationCount(String input, int iterationCount) {
+        final String[] data = Ut.splitStringWithEmptyLines(input);
+
+        final int[] enhancementAlgorithm = parseEnhancementAlgorithm(data[0]);
+        Map<Position, Integer> image = parseImage(data[1], iterationCount);
+
+        for (int i = 0; i < iterationCount; i++) {
+            Map<Position, Integer> newImage = new HashMap<>();
+
+            for (Position pixel : image.keySet()) {
+                newImage.put(pixel,
+                        enhancementAlgorithm[
+                                getOutputPixelIndex(pixel, image, enhancementAlgorithm[0] == 0 ? 0 : i % 2)
+                                ]
+                );
+            }
+
+            image = newImage;
+        }
+
+        int litPixels = 0;
+
+        for (Integer pixelValue : image.values()) {
+            litPixels += pixelValue;
+        }
+
+        return litPixels;
+    }
+
+    private int[] parseEnhancementAlgorithm(String algorithmData) {
+        final int[] enhancementAlgorithm = new int[algorithmData.length()];
+
+        for (int i = 0; i < algorithmData.length(); i++) {
+            enhancementAlgorithm[i] = algorithmData.charAt(i) == '#' ? 1 : 0;
+        }
+
+        return enhancementAlgorithm;
+    }
+
+    private Map<Position, Integer> parseImage(String imageData, int padding) {
+        final String[] lines = Ut.splitMultilineString(imageData);
+        final Map<Position, Integer> image = new HashMap<>();
+
+        for (int y = 0; y < lines.length; y++) {
+            for (int x = 0; x < lines[y].length(); x++) {
+                image.put(new Position(y, x), lines[y].charAt(x) == '#' ? 1 : 0);
+            }
+        }
+
+        for (int y = -padding; y < lines.length + padding; y++) {
+            for (int x = -padding; x < lines.length + padding; x++) {
+                image.putIfAbsent(new Position(y, x), 0);
+            }
+        }
+
+        return image;
+    }
+
+    private int getOutputPixelIndex(Position pixel, Map<Position, Integer> image, int defaultValue) {
+        int outputPixelIndex = 0;
+
+        for (Position neighbour : pixel.getNeighbours(OMNIDIRECTIONAL_AND_SELF)) {
+            outputPixelIndex *= 2;
+            outputPixelIndex += image.getOrDefault(neighbour, defaultValue);
+        }
+
+        return outputPixelIndex;
+    }
+}
--- aoc2021/src/main/java/thoenluk/aoc2021/ut/Position.java
+++ aoc2021/src/main/java/thoenluk/aoc2021/ut/Position.java
@@ -1,12 +1,14 @@
 package thoenluk.aoc2021.ut;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public record Position(int y, int x) {
 
-    public Set<Position> getNeighbours(NeighbourDirection neighbourDirection) {
-        Set<Position> neighbours = new HashSet<>();
+    public List<Position> getNeighbours(NeighbourDirection neighbourDirection) {
+        List<Position> neighbours = new LinkedList<>();
         for (Position direction : neighbourDirection.getDirections()) {
             neighbours.add(offsetBy(direction));
         }
@@ -18,14 +20,20 @@ public record Position(int y, int x) {
     }
 
     public enum NeighbourDirection {
-        CARDINAL(Set.of(new Position(-1, 0), new Position(0, -1), new Position(0, 1), new Position(1, 0))),
-        DIAGONAL(Set.of(new Position(-1, -1), new Position(-1, 1), new Position(1, -1), new Position(1, 1))),
-        OMNIDIRECTIONAL(Set.of(new Position(-1, 0), new Position(0, -1), new Position(0, 1), new Position(1, 0),
-                                new Position(-1, -1), new Position(-1, 1), new Position(1, -1), new Position(1, 1)));
+        CARDINAL(List.of(new Position(-1, 0), new Position(0, -1), new Position(0, 1), new Position(1, 0))),
+        DIAGONAL(List.of(new Position(-1, -1), new Position(-1, 1), new Position(1, -1), new Position(1, 1))),
+        OMNIDIRECTIONAL(List.of(new Position(-1, -1), new Position(-1, 0), new Position(-1, 1),
+                                new Position(0, -1),                            new Position(0, 1),
+                                new Position(1, -1), new Position(1, 0), new Position(1, 1)
+        )),
+        OMNIDIRECTIONAL_AND_SELF(List.of(new Position(-1, -1), new Position(-1, 0), new Position(-1, 1),
+                                        new Position(0, -1), new Position(0, 0), new Position(0, 1),
+                                        new Position(1, -1), new Position(1, 0), new Position(1, 1)
+        ));
 
-        private final Set<Position> directions;
+        private final List<Position> directions;
 
-        /* private */ NeighbourDirection(Set<Position> directions) {
+        /* private */ NeighbourDirection(List<Position> directions) {
             this.directions = directions;
         }
 
@@ -33,7 +41,7 @@ public record Position(int y, int x) {
             return new int[]{y, x};
         }
 
-        public Set<Position> getDirections() {
+        public List<Position> getDirections() {
             return directions;
         }
     }
